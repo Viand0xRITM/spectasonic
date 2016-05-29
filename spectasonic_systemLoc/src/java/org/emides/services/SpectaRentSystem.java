f7b211ca950f1a49619cfff63bec49fdf6f043d5
sbosquet <>
2016-05-29 20:04:39 +0200

    begining of createOrder method

--- spectasonic_systemLoc/src/java/org/emides/services/SpectaRentSystem.java
+++ spectasonic_systemLoc/src/java/org/emides/services/SpectaRentSystem.java
@@ -1,12 +1,14 @@
 package org.emides.services;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
@@ -67,8 +69,12 @@ public class SpectaRentSystem {
     @GET
     @Path("getCustomer/{id}")
     @Produces({"application/json"})
-    public String getCustomerData(@PathParam("id") Integer id) {
-        String response = "ERROR during request";
+    public String getCustomer(@PathParam("id") Integer id) {
+        return getCustomerData(id);
+    }
+    
+    private String getCustomerData(int id){
+        String response = "ERROR during request" + id;
         String uri = "http://localhost:8080/spectasonic_location/webresources/clients/" + id;
         
         try{
@@ -190,41 +196,34 @@ public class SpectaRentSystem {
     
     @POST
     @Path("createOrder/{customerID}/{productID}/{productQuantity}")
-    @Produces({"application/json"})
-    public String createOrder(@PathParam("id") Integer customerID, @PathParam("id") Integer productID, @PathParam("id") Integer productQuantity) {
-        /*{
-        "commandeClientId": {
-        "clientsAdresse": "Rue de l'EPSI",
-        "clientsId": 1,
-        "clientsNom": "N'Zi",
-        "clientsPrenom": "David"
-        },
-        "commandeDate": "2016-05-30T00:00:00+02:00",
-        "commandeId": 3
-        }*/
-        
-        //String customer = getCustomerData(customerID);
-        String customer = "{\"clientsAdresse\": \"Rue de l'EPSI\",\"clientsId\": 1,\"clientsNom\": \"N'Zi\",\"clientsPrenom\": \"David\"}";
-        String response = "ERROR during request";
+    @Produces({"application/text"})
+    public String createOrder(@PathParam("customerID") Integer customerID, @PathParam("productID") Integer productID, @PathParam("productQuantity") Integer productQuantity) {      
+        String customer = getCustomerData(customerID);
+        String response = "HTTP status : ";
+        int errorCode = 0;
         String request;
         String uri = "http://localhost:8080/spectasonic_location/webresources/commande/";
         
-        request = "{\"commandeClientId:\":";
+        request = "{\"commandeClientId\":";
         request += customer + ",";
-        request += "\"commandeDate:\":" + "\"2016-05-30T00:00:00+02:00\",";
-        request += "\"commandeId:\":" + 4 + "}";
+        request += "\"commandeDate\":" + "\"2016-05-30T00:00:00+02:00\",";
+        request += "\"commandeId\":" + 10 + "}";
        
         try{
             URL url = new URL(uri);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
-            connection.setRequestProperty("Accept", "application/json");
+            connection.setRequestProperty("Content-Type", "application/json");
             connection.setDoOutput(true);
             
-            OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream());
+            connection.connect();
+            
+            OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
             wr.write(request);
             wr.flush();
             
+            errorCode = connection.getResponseCode();
+                      
             connection.disconnect();
         } catch (MalformedURLException ex) {
             Logger.getLogger(SpectaRentSystem.class.getName()).log(Level.SEVERE, null, ex);
@@ -233,5 +232,37 @@ public class SpectaRentSystem {
         }
         
         return request;
+        //return response + errorCode;
+    }
+    
+    private String createOrderContent(int orderID, int productID, int quantity) {
+        String response = "HTTP status : ";
+        int errorCode = 0;
+        String request = "";
+        String uri = "http://localhost:8080/spectasonic_location/webresources/commande/";
+       
+        try{
+            URL url = new URL(uri);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+            
+            connection.connect();
+            
+            OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+            wr.write(request);
+            wr.flush();
+            
+            errorCode = connection.getResponseCode();
+                      
+            connection.disconnect();
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(SpectaRentSystem.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(SpectaRentSystem.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return response;
     }
 }
